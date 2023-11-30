f9efd5f8256536f7f12814374f1d57fa46f79f56
Abir <abirmahmud221@gmail.com>
2023-11-30 16:58:24 +0600

    almost added jwt and isAdmin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require("jsonwebtoken");
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -33,6 +34,30 @@ async function run() {
     const userCollection = client.db("oscorpTech").collection("users");
     const assetCollection = client.db("oscorpTech").collection("assets");
 
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: "1hr",
+        });
+        res.send({ token });
+      });
+
+    // middlewares
+    const verifyToken = (req, res, next) => {
+        console.log("inside verify token", req.headers);
+        if (!req.headers.authorization) {
+          return res.status(401).send({ message: "forbidden access" });
+        }
+        const token = req.headers.authorization.split(" ")[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+          if (err) {
+            return res.status(401).send({ message: "forbidden access" });
+          }
+          req.decoded = decoded;
+          next();
+        });
+      };
 
     
     //user related api
@@ -40,6 +65,22 @@ async function run() {
         const result = await userCollection.find().toArray();
         res.send(result);
       });
+
+      // jwt admin related work
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+        const email = req.params.email;
+        if (email !== req.decoded.email) {
+          return res.status(403).send({ message: "unauthorized access" });
+        }
+        const query = { email: email };
+        const user = await userCollection.findOne(query);
+        let admin = false;
+        if (user) {
+          admin = user?.role === "admin";
+        }
+        res.send({ admin });
+      });
+
 
     app.post("/users", async (req, res) => {
         const user = req.body;
